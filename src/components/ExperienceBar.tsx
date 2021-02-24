af78525368af7db4565dd2f2e61beb51ae8bc33d
Johnatan Brayan <johnatanbrayan1@gmail.com>
2021-02-23 23:07:54 -0300

    Configure and creating new components ExperienceBar

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+
+export function ExperienceBar() {
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 XP</span>
+      <div>
+        <div style={{ width: "60%" }} />
+
+        <span className={styles.currentExperience} style={{ left: "60%" }}>
+          300 XP
+        </span>
+      </div>
+      <span>600 XP</span>
+    </header>
+  );
+}
+
+export default ExperienceBar;
